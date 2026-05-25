f78008ec79a13183b131cac7c3b0f769845507bb
Andrew Hu <andrewhu32009@gmail.com>
2026-05-25 16:29:41 -0400

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        found = False
+        def checkWord(r, c, board, index):
+            if not self.isValid(r, c, board) or index >= len(word) or board[r][c] == "#":
+                return False
+
+            val = board[r][c]
+
+            if val != word[index]:
+                return False
+
+            if index == len(word) - 1:
+                return True
+
+            board[r][c] = "#" 
+        
+            found = (checkWord(r + 1, c, board, index + 1) or
+                checkWord(r - 1, c, board, index + 1) or
+                checkWord(r, c + 1, board, index + 1) or
+                checkWord(r, c - 1, board, index + 1))
+
+            board[r][c] = val
+            return found
+
+            
+            
+        for r in range(len(board)):
+            for c in range(len(board[r])):
+                found = checkWord(r, c, board, 0)
+                if found:
+                    return True
+
+        return found
+
+
+    def isValid(self, r, c, board):
+        return r >= 0 and r < len(board) and c >= 0 and c < len(board[r])
