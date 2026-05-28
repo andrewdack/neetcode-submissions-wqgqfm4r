49bead24977ceb1a4f8046163d312e777ae0ef53
Andrew Hu <andrewhu32009@gmail.com>
2026-05-28 14:36:36 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = []
+        table = {} # len 26 tuple -> list of strings
+
+        for word in strs:
+            countList = [0 for _ in range(26)]
+
+            for char in word:
+                countList[ord(char) - ord('a')] += 1
+
+            countTuple = tuple(countList)
+            table[countTuple] = table.get(countTuple, []) + [word]
+        
+        for s in table:
+            result.append(table[s])
+
+        return result
